c9e5811ec0eec7a26d8e77654dff66a8ad8babb0
Banaiche Abdelali <banaiche.abdelali@gmail.com>
2023-07-03 16:44:26 +0100

    feat-commit for creating Caisse and Rapprochement !

---
+++ reclamation/src/main/java/ma/atos/reclamation/dto/Rapprochement.java
@@ -0,0 +1,52 @@
+package ma.atos.reclamation.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Rapprochement {
+
+    private Caisse caisse;
+    private LocalDateTime date;
+    private boolean ecart;
+
+    private BigDecimal montantEcart;
+
+    public Rapprochement(Caisse caisse, LocalDateTime date, boolean ecart, BigDecimal montantEcart) {
+        this.caisse = caisse;
+        this.date = date;
+        this.ecart = ecart;
+        this.montantEcart = montantEcart;
+    }
+
+    public Caisse getCaisse() {
+        return caisse;
+    }
+
+    public void setCaisse(Caisse caisse) {
+        this.caisse = caisse;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public boolean isEcart() {
+        return ecart;
+    }
+
+    public void setEcart(boolean ecart) {
+        this.ecart = ecart;
+    }
+
+    public BigDecimal getMontantEcart() {
+        return montantEcart;
+    }
+
+    public void setMontantEcart(BigDecimal montantEcart) {
+        this.montantEcart = montantEcart;
+    }
+}
