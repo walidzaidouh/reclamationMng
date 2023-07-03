38a273d1076e2f14113d28a744fa1906ad48b263
cdm-e7200 <walidzaidouh@gmail.com>
2023-07-03 10:26:12 +0100

    first commit

---
+++ reclamation/src/main/java/ma/atos/reclamation/ReclamationApplication.java
@@ -0,0 +1,13 @@
+package ma.atos.reclamation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReclamationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReclamationApplication.class, args);
+	}
+
+}
