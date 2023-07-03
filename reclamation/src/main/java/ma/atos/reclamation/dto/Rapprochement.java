08e9c7e7f46ed40578063f5320b0438120508c16
Banaiche Abdelali <banaiche.abdelali@gmail.com>
2023-07-03 15:30:15 +0100

    feat-commit for creating DTO'

---
+++ reclamation/src/main/java/ma/atos/reclamation/dto/Rapprochement.java
@@ -0,0 +1,15 @@
+package ma.atos.reclamation.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Rapprochement {
+
+    private LocalDate date;
+    private  boolean ecart;
+
+    private BigDecimal montantecart;
+
+
+
+}
