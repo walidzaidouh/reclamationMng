711a48bdbe041b81bd8e6fea37e382858a5dc3a2
Reda Wahib <redawahib99@gmail.com>
2023-07-05 16:18:55 +0100

    Commit Facture Entity

--- reclamation/src/main/java/ma/atos/reclamation/entities/Facture.java
+++ reclamation/src/main/java/ma/atos/reclamation/entities/Facture.java
@@ -1,11 +1,19 @@
 package ma.atos.reclamation.entities;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
+@Getter
+@Setter
+@AllArgsConstructor
 @Entity
 public class Facture {
     @Id
@@ -27,72 +35,4 @@ public class Facture {
 
     private BigDecimal montant;
     private String statut;
-
-    public Facture() {}
-
-    public Facture(String reference, List<Transaction> transactionList, Client client, LocalDateTime dateFacture, LocalDateTime dateEcheance, BigDecimal montant,String statut) {
-        this.reference = reference;
-        this.transactionList = transactionList;
-        this.client = client;
-        this.dateFacture = dateFacture;
-        this.dateEcheance = dateEcheance;
-        this.montant = montant;
-        this.statut = statut;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
-
-    public List<Transaction> getTransactionList() {
-        return transactionList;
-    }
-
-    public void setTransactionList(List<Transaction> transactionList) {
-        this.transactionList = transactionList;
-    }
-
-    public Client getClient() {
-        return client;
-    }
-
-    public void setClient(Client client) {
-        this.client = client;
-    }
-
-    public LocalDateTime getDateFacture() {
-        return dateFacture;
-    }
-
-    public void setDateFacture(LocalDateTime dateFacture) {
-        this.dateFacture = dateFacture;
-    }
-
-    public LocalDateTime getDateEcheance() {
-        return dateEcheance;
-    }
-
-    public void setDateEcheance(LocalDateTime dateEcheance) {
-        this.dateEcheance = dateEcheance;
-    }
-
-    public BigDecimal getMontant() {
-        return montant;
-    }
-
-    public void setMontant(BigDecimal montant) {
-        this.montant = montant;
-    }
 }
