3ca36fab3ce0709eba7bc3e0b8a042530e163870
oumeslakht tarik <oumeslakht.tarik@gmail.com>
2023-07-05 16:21:04 +0100

    Feat - commit for creating dto's

--- reclamation/src/main/java/ma/atos/reclamation/entites/Transaction.java
+++ reclamation/src/main/java/ma/atos/reclamation/entites/Transaction.java
@@ -1,64 +1,37 @@
 package ma.atos.reclamation.entites;
 
-import ma.atos.reclamation.enums.TypePaiement;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import ma.atos.reclamation.enums.TypePaiement;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Transaction {
 
+@Id
+
+@GeneratedValue(strategy =  GenerationType.SEQUENCE)
+
+    private long id;
     private String reference;
     private TypePaiement typePaiement;
     private BigDecimal montant;
     private LocalDateTime date;
     private boolean isRapprochement;
 
-    public Transaction(String reference, TypePaiement typePaiement, BigDecimal montant, LocalDateTime date, boolean isRapprochement) {
-        this.reference = reference;
-        this.typePaiement = typePaiement;
-        this.montant = montant;
-        this.date = date;
-        this.isRapprochement = isRapprochement;
-    }
 
-    public String getReference() {
-        return reference;
-    }
+    @ManyToOne
+    @JoinColumn(name = "facture_id")
+    private Facture facture;
+    @Enumerated(EnumType.STRING)
+    private String Type;
 
 
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
-
-    public TypePaiement getTypePaiement() {
-        return typePaiement;
-    }
-
-    public void setTypePaiement(TypePaiement typePaiement) {
-        this.typePaiement = typePaiement;
-    }
-
-    public BigDecimal getMontant() {
-        return montant;
-    }
-
-    public void setMontant(BigDecimal montant) {
-        this.montant = montant;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public boolean isRapprochement() {
-        return isRapprochement;
-    }
-
-    public void setRapprochement(boolean rapprochement) {
-        isRapprochement = rapprochement;
-    }
 }
