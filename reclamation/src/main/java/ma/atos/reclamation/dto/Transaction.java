0a95455b1ff945a2e908ad952bad8a14d97cf980
oumeslakht tarik <oumeslakht.tarik@gmail.com>
2023-07-05 15:28:16 +0100

    Feat - commit for creating dto's

--- reclamation/src/main/java/ma/atos/reclamation/dto/Transaction.java
+++ reclamation/src/main/java/ma/atos/reclamation/dto/Transaction.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 
 public class Transaction {
 
+    //private Long id;
     private String reference;
     private TypePaiement typePaiement;
     private BigDecimal montant;
