4cc9452476fef712a62870a0aeb74a740b8102dd
Reda Wahib <redawahib99@gmail.com>
2023-07-06 12:23:56 +0100

    Commit Facture Entity

---
+++ reclamation/src/main/java/ma/atos/reclamation/repositories/FactureRepository.java
@@ -0,0 +1,23 @@
+package ma.atos.reclamation.repositories;
+
+import ma.atos.reclamation.entites.Facture;
+import ma.atos.reclamation.entites.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FactureRepository extends JpaRepository {
+
+    List<Facture> findAll();
+
+    public Facture findByReference(String reference);
+
+    @Modifying
+    @Query("UPDATE Facture f SET f.statut = 'payée' WHERE f.id = :id")
+    void payerFacture(@Param("id") Long id);
+}
--- reclamation/src/main/java/ma/atos/reclamation/services/FactureService.java
+++ reclamation/src/main/java/ma/atos/reclamation/services/FactureService.java
@@ -1,12 +1,14 @@
 package ma.atos.reclamation.services;
 
+import ma.atos.reclamation.dto.AgenceDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public interface FactureService {
-    // Méthode pour afficher les factures
-    void afficherFactures();
-    //List<Facture> factures = factureRepository.findAll();
+public interface Facture {
+
+    public List<Facture> list();
 
     // Méthode pour effectuer le paiement d'une facture
     void payerFacture(String reference);
