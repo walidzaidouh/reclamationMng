fa8c6fb7b4c5a5fba75d33b4ba4e66e18df9fa8a
frindy Abdelkoudouss <abdelkoudous.frindy@atos.net>
2023-07-06 12:29:28 +0100

    feat-commit for creating DTO'

---
+++ reclamation/src/main/java/ma/atos/reclamation/repositories/DistributeurRepository.java
@@ -0,0 +1,16 @@
+package ma.atos.reclamation.repositories;
+
+
+import ma.atos.reclamation.dto.Distributeur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DistributeurRepository extends JpaRepository <Distributeur, Long> {
+    Optional<Distributeur> findById(@Param("code") String code);
+
+
+}
