89ca1fa56ba87152fb57962c7857e86a71185a17
achraf taki <achraftaki57@gmail.com>
2023-07-07 16:19:26 +0100

    feat-commit for modifications

---
+++ reclamation/src/main/java/ma/atos/reclamation/exceptions/ControllerAdviser.java
@@ -0,0 +1,19 @@
+package ma.atos.reclamation.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+@ControllerAdvice
+public class ControllerAdviser extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleNotFoundEntite(Exception e){
+        System.out.println("test");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+}
