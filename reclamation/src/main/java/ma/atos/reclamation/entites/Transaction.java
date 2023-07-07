78f676cdabc93ae5ba8e7bde04fab5deba56fe31
a883002 <ab.fikri@hotmail.com>
2023-07-07 13:02:38 +0100

    Feat-push

---
+++ reclamation/src/main/java/ma/atos/reclamation/entites/Transaction.java
@@ -0,0 +1,32 @@
+package ma.atos.reclamation.entites;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import ma.atos.reclamation.enums.TypePaiement;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    private String reference;
+    @Enumerated(EnumType.STRING)
+    private TypePaiement typePaiement;
+
+    private BigDecimal montant;
+    private LocalDateTime date;
+    private boolean isRapprochement;
+    @ManyToOne
+    @JoinColumn(name = "facture_id")
+    private Facture facture;
+
+}
